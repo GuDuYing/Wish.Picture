6bb7bd8bf99c091ee0c7873f893592c9a9506808
fengrong <fengrong@bailuntec.com>
2019-10-17 19:01:22 +0800

    增加用户登陆页面

--- Wish.Picture/Wish.Picture.Web/Controllers/UserController.cs
+++ Wish.Picture/Wish.Picture.Web/Controllers/UserController.cs
@@ -12,5 +12,10 @@ namespace Wish.Picture.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
