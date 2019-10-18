ca22e22968da93709611db119c135daf4290c185
fengrong <fengrong@bailuntec.com>
2019-10-18 14:26:23 +0800

    增加图片库页面

---
+++ Wish.Picture/Wish.Picture.Web/Controllers/PictureController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wish.Picture.Web.Controllers
+{
+    public class PictureController : Controller
+    {
+        public IActionResult Index()
+        {
+            var loginUserName = HttpContext.Session.GetString("LoginUserName");
+            if (!string.IsNullOrEmpty(loginUserName))
+            {
+                ViewData["loginUserName"] = loginUserName;
+            }
+            return View();
+        }
+    }
+}
